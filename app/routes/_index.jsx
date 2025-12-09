cd9f3626b0436a5a538a9f37e3fb6cbab0fd3ef3
kjgilzow <112286917+kjgilzow@users.noreply.github.com>
2025-12-08 17:23:13 -0800

    fixed index navigate issue warning

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+import { Navigate } from "react-router";
 import {Await, useLoaderData, Link} from 'react-router';
 import {Suspense} from 'react';
 import {Image} from '@shopify/hydrogen';
